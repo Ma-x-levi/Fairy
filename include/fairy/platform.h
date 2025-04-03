21e901c6e872813d65c04ea3c323dc5db9b01b1b
levi <liweijun@ziyanuav.com>
2025-04-03 18:17:41 +0800

    first commit

---
+++ include/fairy/platform.h
@@ -0,0 +1,21 @@
+#ifndef FAIRY_INCLUDE_FAIRY_PLATFORM_H_
+#define FAIRY_INCLUDE_FAIRY_PLATFORM_H_
+
+#include "type.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+//typedef struct {
+//
+//} T_
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FAIRY_INCLUDE_FAIRY_PLATFORM_H_ */
---
+++ include/fairy/types.h
@@ -0,0 +1,17 @@
+#ifndef FAIRY_INCLUDE_FAIRY_TYPES_H_
+#define FAIRY_INCLUDE_FAIRY_TYPES_H_
+
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FAIRY_INCLUDE_FAIRY_TYPES_H_ */
